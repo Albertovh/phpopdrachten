9e58f41f2f3c438d3b8fcdf5cefec92b57c64834
Alberto van hoek <Alberto.avh@gmail.com>
2020-02-19 22:03:17 +0100

    opdracht 2.1

---
+++ hoofdstuk2/opdracht2.1/opdracht2.1.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * User: alberto
+ * Date: 17-02-2020
+ * Time: 17-17
+ * File: opdracht2.1.php
+ */
+?>
+
+<html>
+
+<head>
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <link rel="icon" href="../CSS/style.css" />
+    <meta charset="utf-8">
+    <title>
+
+    </title>
+</head>
+
+
+    <header>
+        <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    </header>
+<body>
+    <aside>
+        <h2>Menu</h2>
+        <ul>
+            <li>Hoofdstuk 2
+                <ul>
+                    <li>
+                        <a href="hoofdstuk2/opdracht2.1">Opdracht 2.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht2.2/opdracht2.2.php">Opdracht 2.2</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+    </main>
+</body>
+</html>
